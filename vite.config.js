ad55cdd6d7912e03244532bc8d071b7cd2e14cd6
ikonnectharshit <ikonnect.harshit@gmail.com>
2026-03-28 22:46:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this from '/isl/' to just '/'
+  base: '/', 
 })
